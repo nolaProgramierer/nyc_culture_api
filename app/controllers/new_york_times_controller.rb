1f59ad41b0bba1383bfb4b4694740e7e0f27442f
Glenn Langdon <glennlangdon@gmail.com>
2015-11-08 12:01:42 -0500

    commit with Jeff

--- app/controllers/new_york_times_controller.rb
+++ app/controllers/new_york_times_controller.rb
@@ -1,26 +1,12 @@
 class NewYorkTimesController < ApplicationController
   def index
-
+    @event = ""
     key = 'f528e2fc1d19595d065143ed0b6aa700:18:26374763'
-    response = HTTParty.get("http://api.nytimes.com/svc/events/v2/listings.json?&filters=category:(#{event})&api-key=#{key}")
+    response = HTTParty.get("http://api.nytimes.com/svc/events/v2/listings.json?&filters=category:(#{@event})&api-key=#{key}")
     # returns a series of arrays
     @events = JSON::parse(response.body)
     # isolates the 'results' array of hashes
     @results = @events['results']
 
   end
-
-  def event
-    "Dance"
-  end
-  # def select_event(event)
-  #   @event = event
-  # end
-
-
-  # def method(event)
-  #   @event = event
-  # end
-
-
 end
