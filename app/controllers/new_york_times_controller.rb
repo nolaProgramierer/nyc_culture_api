fad946ff0e21f5c6fa04d2f76049b902e4517bd5
Glenn Langdon <glennlangdon@gmail.com>
2015-11-10 09:53:37 -0500

    Damon's changes

--- app/controllers/new_york_times_controller.rb
+++ app/controllers/new_york_times_controller.rb
@@ -1,15 +1,23 @@
 class NewYorkTimesController < ApplicationController
+  NYT_API_KEY = 'f528e2fc1d19595d065143ed0b6aa700:18:26374763'
+  NYT_BASE_URL = "http://api.nytimes.com/svc/events/v2/listings.json"
+
   def index
-  # @events = "Theater"
-    key = 'f528e2fc1d19595d065143ed0b6aa700:18:26374763'
-    response = HTTParty.get("http://api.nytimes.com/svc/events/v2/listings.json?&filters=category:(#{get_event})&api-key=#{key}")
-    # returns a series of arrays
-    @events = JSON::parse(response.body)
-    # isolates the 'results' array of hashes
+    event_string =  if params[:event_type].present?
+                      params[:event_type]
+                    else
+                      "Theater"
+                    end
+    @events = query_nyt(event_string)
     @results = @events['results']
   end
 
-  def get_event
-    params[:radios1]
+  private
+
+  def query_nyt(event)
+    url_params  = "?&filters=category:(#{event})&api-key=#{NYT_API_KEY}"
+    url         = NYT_BASE_URL + url_params
+    response = HTTParty.get(url)
+    JSON.parse response.body
   end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,5 @@
 Rails.application.routes.draw do
+  post "/events", to: "new_york_times#index"
   root 'new_york_times#index'
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
